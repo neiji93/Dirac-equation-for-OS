dcce0d40ecba91ae5ea9c1a7d086e49511c65533
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2025-01-14 07:51:22 +0100

    Update FormalSeriesRandomnessGenerator.h

--- FormalSeriesRandomnessGenerator.h
+++ FormalSeriesRandomnessGenerator.h
@@ -5,3 +5,21 @@
 //partial tensors ro be used. then the hankel transforms
 //turned to physics wont hvve to worry about signals
 //because a point is not physically bases
+
+
+//as point is kot physically based, we can call motherboard
+//signals intrinsics because ....
+class motherboardChipsSignals
+{}
+
+class generateRandomnessProvider
+{
+public:
+
+  int getSignalFromRzndomGameFormHere();
+
+  //as we used singlas from.chips, we can stop them
+  //with async keyword. but there must be a relationship 
+  //with formal series. then, it will take time. so reboot the system.
+  bool stopOSnow();
+}
