505d6b9e6c354a25bcf01f461f705896cf4731fa
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2025-01-14 07:45:21 +0100

    Create FormalSeriesRandomnessGenerator.h

---
+++ FormalSeriesRandomnessGenerator.h
@@ -0,0 +1,7 @@
+
+//as formal series can be applied to physics
+//several timed in my stuffs, it may be a good starting point 
+//it will prevent other equations like partizl equation or
+//partial tensors ro be used. then the hankel transforms
+//turned to physics wont hvve to worry about signals
+//because a point is not physically bases
