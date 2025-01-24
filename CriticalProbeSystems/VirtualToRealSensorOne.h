b1a4206ba1549bd6afe84b801728b3806924e4cd
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2025-01-24 10:59:41 +0100

    Create VirtualToRealSensorOne.h

---
+++ CriticalProbeSystems/VirtualToRealSensorOne.h
@@ -0,0 +1,3 @@
+//we can do that as ram, cpu and hd are not used for the kernel base !
+//so let's patch...as i did in my game engine. Using game engine logics can lead to really complex forms of programs.
+//Then, the complexity will remain in the physic engine !
