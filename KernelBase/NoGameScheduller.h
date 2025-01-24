f13804da055bb3852bc641cd0d97539f935e58bd
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2025-01-24 10:44:35 +0100

    Create NoGameScheduller.h

---
+++ KernelBase/NoGameScheduller.h
@@ -0,0 +1,2 @@
+//the latest possible shader in the cgi 2026 repository suggests that we can still use hitchin morphism. Then, we don't need mmean field games theory here.
+//we can use linux kernel scheduller because we don't even need my game engine scheduller.
