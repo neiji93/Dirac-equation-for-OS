be437bb31f2fcdb26b18e3d28c505525371de5fb
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2025-01-24 08:59:07 +0100

    Update AutoKernelCompilerForceLogicsFromCGIPatternOneHack.h

--- Programs/AutoKernelCompilerForceLogicsFromCGIPatternOneHack.h
+++ Programs/AutoKernelCompilerForceLogicsFromCGIPatternOneHack.h
@@ -1,2 +1,6 @@
 //There is no real properties of the first hack test. Then, without ddelegates logics, it will be good to use immediately
 //models with patterns which fail.
+
+
+
+//if patterns fail to restore optimizer, then, we don't need to use the RAM. It means that ..maybe there is a link with my first or latest chess engine !
